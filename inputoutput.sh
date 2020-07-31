d97a59089e99e57f9e94f566ea4c491786e40d76
Parrot Live user <user@localhost.localdomain>
2020-07-31 15:10:48 +0000

    input output file

---
+++ inputoutput.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -e "Enter Your Name: "
+#storing Variable in a name
+read name
+echo "Your Name is $name"
---
+++ makeget.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir  Get
+cd Get
+touch readme.txt
+"make this read as possible as soon"
