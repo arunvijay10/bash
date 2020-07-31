1de48cc0a16f81c33a4e45fa257e3900bcab7970
Parrot Live user <user@localhost.localdomain>
2020-07-31 14:19:08 +0000

    fully nmap

--- building.sh
+++ building.sh
@@ -4,5 +4,16 @@ echo -e $change
 figlet  nmap
 echo "what do you want nmap or zenmap"
 echo -e " choose \n1 for nmap \n2 for zenmap"
+read num
+if [ $num -eq 1 ]
+then
+	echo "you have choosen nmap"
+	echo  "nmap is  starting ..."
+	sleep 2
+	echo  "Enter The ip To Scan"
+	read  ip
+	nmap $ip
+else
+	echo -e "you have choosen zenmap"
 
-
+fi
