31faac51cfcd7d5789394b64fee15d5d2e8afd6e
Parrot Live user <user@localhost.localdomain>
2020-08-05 08:32:18 +0000

    ip locator underdevlopment

---
+++ iplocator.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+figlet "iplocator"
+colour="\e[33m"
+echo -e "$colour"
+echo -e "1 for ip details \n2 for geolocation"
+echo  "choose option"
+read ch
+change="\e[102"
+echo -e "$change"
+if [ $ch -eq 1 ]
+then
+	echo "Ip Detail's are starting..."
+	sleep 2
+elif [ $ch -eq 2 ]
+then
+	echo "Geolocation starting.."
+	sleep 2
+else
+	echo "Invalid Option  From Try Valid Option's !"
+fi
